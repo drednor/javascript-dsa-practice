2f4097c7c27b3c980a8c82cc46b82f897a4b7c9e
drednor <singh.msoe@gmail.com>
2026-03-14 16:02:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const prompt = require("prompt-sync")()
+
+// hello 
+/*console.error("this is an error")
+console.warn("this is a warning ")
+console.log("something like a log")*/
+
+// const v = 64
+// console.log(`${v}`)
+
+//const y = prompt("type something : ")
+
+//console.log(`${y}`)
+
+// variable and data types 
+
+/*
+
+DATA TYPES 
+string  
+boolean
+number 
+undefined - can be assigned later  
+null - will always be null 
+bigint 
+symbol
+
+VARIABLE
+var - functionscope, can be changed later - function scope
+let - can be changed later on = blockscope
+const - will be the same through out the program. will not change 
+
+*/
+
+const x = 2
+const y = "doodle"
+console.log(x + y)
